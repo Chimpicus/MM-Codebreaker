a6219d9d100f21b981d55a23f382e978155ec11b
Chimpicus <caseyboyes@gmail.com>
2020-11-06 10:24:57 +0000

    Update

--- script.js
+++ script.js
@@ -1,12 +1,7 @@
-//need to make a function that onclick cycles through numbers 1 to 6. UPDATE - Complete -
-//need to make a function that converts numbers to colors and assigns them to the background color of the div. UPDATE - complete -
-// need to create a function to change the next enter button to green. UPDATE - complete - 
-
 
 //settings
-let colorblindMode = true
+let colorblindMode = false
 let difficulty = 5
-
 
 let number = 0
 currentRow = 0 // to set active row
@@ -14,8 +9,8 @@ let color = ['', 'red', 'blue', 'limegreen', 'yellow', 'orange', 'magenta' , 'cy
 
 window.onLoad = gameStart()
 
-//function gameStart() {
-    //document.getElementById("enter_0").style.background='lime'};
+function gameStart() {
+    document.getElementById("enter_0").style.background='lime'};
 
 
 
@@ -35,6 +30,7 @@ function colorCycle(id) {
         }
         else {
         number = 1;
+        
     if(colorblindMode === true) {
         guess.innerText = number;
         };
@@ -67,10 +63,4 @@ function colorCycle(id) {
     };
         
 
-        // enter button skips a row if double clicked, needs to be adressed.
-        // need to create a function that generates a 4 digit code for the answer.
-        // need to create a function that compares current row pins to answer on submit.
-        // need to creat a function that provides feedback to feedback pins.
-        // game start function that sets the answer and also turns frist row submit button on.
-
-        // could store pin values in an object to solve the small bug of continuing number count over different pins.
+        
